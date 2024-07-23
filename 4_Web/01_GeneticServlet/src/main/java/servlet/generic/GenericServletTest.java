0f5350543cda1d9d52a515a5ece981f488a25ff8
songjunho95 <songpro3290@naver.com>
2024-07-23 17:15:24 +0900

    1 - 17 : Java

---
+++ 4_Web/01_GeneticServlet/src/main/java/servlet/generic/GenericServletTest.java
@@ -0,0 +1,35 @@
+package servlet.generic;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+
+
+public class GenericServletTest extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		
+		// /generic 요청 받았다.. 치고
+		// 바로 응답! 클라이언트 브라우저로 응답
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>Hello, GenericServletTest!</h1>");
+		out.println("</body></html>");
+		
+		out.close();
+		
+		
+	}
+
+}
